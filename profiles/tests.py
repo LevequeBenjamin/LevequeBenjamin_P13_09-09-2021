b09ec3d656fb65f51a7c250e1f623e30313357fc
bosso <bosso27@live.fr>
2021-09-17 13:02:08 +0200

    clean code

--- profiles/tests.py
+++ profiles/tests.py
@@ -14,10 +14,14 @@ def test_profile_index_view(client):
 
 @pytest.mark.django_db
 def test_profile_view(client):
-    user = User.objects.create(username='Sangoku', first_name='Goku', last_name="San",
-                               email='sangoku@test.com')
+    user = User.objects.create(
+        username='Sangoku',
+        first_name='Goku',
+        last_name="San",
+        email='sangoku@test.com'
+    )
     Profile.objects.create(favorite_city='Annecy', user_id=user.id)
-    url = reverse('profile', args=(user.username, ))
+    url = reverse('profile', args=(user.username,))
     response = client.get(url)
     assert b'Sangoku' in response.content
     assert response.status_code == 200
